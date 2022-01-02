f65c52a4910eb74c5384bc7d95a2a69887a9331b
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2022-01-02 13:24:12 +0100

    Some extra struggle with initialization of vector<Elements>.

--- Pokemon/Utils.cpp
+++ Pokemon/Utils.cpp
@@ -1,1 +1,12 @@
 #include "Utils.h"
+#include "Elements.h"
+
+bool containsInVector(const vector<Elements>& arr, const Elements& key)
+{
+	for (size_t i = 0; i < arr.size(); i++) {
+		if (key == arr[i]) {
+			return true;
+		}
+	}
+	return false;
+};
--- Pokemon/Utils.h
+++ Pokemon/Utils.h
@@ -3,13 +3,17 @@
 
 using namespace std;
 
+/* - Simplify method to evade compile error C2440
+
 template<typename T>
 bool containsInVector(const vector<T>& arr, const T& key)
 {
-	for (int i = 0; i < arr.size(); i++) {
+	for (size_t i = 0; i < arr.size(); i++) {
 		if (key == arr[i]) {
 			return true;
 		}
 	}
 	return false;
 };
+*/
+bool containsInVector(const vector<Elements>&, const Elements&);
