83e7118e024ce92810896abf79b4218e337cb34b
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2022-01-02 13:04:09 +0100

    Struggling with some compilation issue on comparison generic types in vector.

--- Pokemon/Pokemon.cpp
+++ Pokemon/Pokemon.cpp
@@ -1,13 +1,32 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
+#include "Elements.h"
 
 using namespace std;
 
 int main()
 {
+    /*
+        MESSAGES
+    */
     const string QUIT_GAME = "Q";
     const string MENU_INPUT = ">> Please enter command or Q for exit.";
     const string GOODBYE = "Thank you for playing.";
+
+    /*
+        INIT
+    */
+    const vector<Elements> universe { 
+        new Elements("WATER"),      new Elements("FIRE"),       new Elements("EARTH"),
+        new Elements("AIR"),        new Elements("ICE"),        new Elements("STEEL")
+    };
+
+
+
+    /*
+        GAMELOOP
+    */
 
     string message = "";
     while (message != QUIT_GAME) {
--- Pokemon/Utils.h
+++ Pokemon/Utils.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <vector>
 
+using namespace std;
+
 template<typename T>
-bool containsInVector(vector<T> array, T key)
+bool containsInVector(const vector<T>& arr, const T& key)
 {
-	for (int i = 0; i < array.size(); i++) {
-		if (key == array[i]) {
+	for (int i = 0; i < arr.size(); i++) {
+		if (key == arr[i]) {
 			return true;
 		}
 	}
