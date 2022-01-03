2bf2dad8b86a97a4b687add19271788b6385e653
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2022-01-04 00:30:52 +0100

    Introduce methods for SpecialAttack.

--- Pokemon/SpecialAttack.h
+++ Pokemon/SpecialAttack.h
@@ -3,8 +3,6 @@
 class SpecialAttack
 {
 protected:
-	
-public:
 	int ownStrengthModifier;
 	int targetStrengthModifier;
 	int ownDexterityModifier;
@@ -13,6 +11,21 @@ public:
 	int maxSpecialPowerDuration;
 	int currentSpecialPowerDuration;
 	bool isActive;
+public:
+	SpecialAttack();
+	int getOwnStrengthModifier() const;
+	void setOwnStrengthModifier(int value);
+	int getTargetStrengthModifier() const;
+	void setTargetStrengthModifier(int value);
+	int getOwnDexterityModifier() const;
+	void setOwnDexterityModifier(int value);
+	int getTargetDexterityModifier() const;
+	void setTargetDexterityModifier(int value);
+	int getMaxSpecialPowerDuration() const;
+	void setMaxSpecialPowerDuration(int value);
+	int getCurrentSpecialPowerDuration() const;
+	void setCurrentSpecialPowerDuration(int value);
+	bool isActive() const;
 	void activate();
 	void reset();
 };
