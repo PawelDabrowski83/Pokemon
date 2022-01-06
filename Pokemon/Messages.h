d7f2b66d84f231cd5ce2ac620c49a227403a3875
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2022-01-07 00:14:45 +0100

    Add some welcome messages.

--- Pokemon/Messages.h
+++ Pokemon/Messages.h
@@ -8,3 +8,5 @@ const string MENU_INPUT = ">> Please enter command or Q for exit.";
 const string GOODBYE = "Thank you for playing.";
 const string BEGIN_ROUND_NO = "Beginning round number ";
 const string HR = "=====================================================================";
+const string WELCOME = "Welcome to generic turn game with non-trademarked creatures.";
+const string MENU_OPTIONS = "Please select yout options by entering proper key.";
--- Pokemon/Pokemon.cpp
+++ Pokemon/Pokemon.cpp
@@ -14,13 +14,13 @@ int main()
         GAMELOOP
     */
     int gameRound = 1;
+    cout << WELCOME << endl;
 
     string message = "";
     while (message != QUIT_GAME) {
         cout << HR << endl;
         cout << BEGIN_ROUND_NO << gameRound++ << endl;
         cout << MENU_INPUT << endl;
-        cout << ELEM_STEEL->attackOn(WATER) << endl;
         
         cin >> message;
 
