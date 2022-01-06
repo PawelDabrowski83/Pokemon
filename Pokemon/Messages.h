d345f4480b230494d128cf2ecd5f0e504ca1e9ef
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2022-01-06 23:56:31 +0100

    Introduce horizon line to divide messages.

--- Pokemon/Messages.h
+++ Pokemon/Messages.h
@@ -6,3 +6,5 @@ using namespace std;
 const string QUIT_GAME = "Q";
 const string MENU_INPUT = ">> Please enter command or Q for exit.";
 const string GOODBYE = "Thank you for playing.";
+const string BEGIN_ROUND_NO = "Beginning round number ";
+const string HR = "=====================================================================";
